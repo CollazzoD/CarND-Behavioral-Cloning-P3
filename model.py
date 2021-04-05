2da8e4628ed0245dda49a36b3a319800448f95ac
CollazzoD <collazzo.d@gmail.com>
2021-04-05 16:22:26 +0200

    Add Dropout layer

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import sklearn
 from sklearn.model_selection import train_test_split
 import cv2
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D, Dropout
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
 import os
@@ -63,6 +63,7 @@ def NVIDIA_Model():
     model.add(Conv2D(64, (3, 3), activation="relu"))
     model.add(Flatten())
     model.add(Dense(100))
+	model.add(Dropout(0.2))
     model.add(Dense(50))
     model.add(Dense(10))
     model.add(Dense(1))
